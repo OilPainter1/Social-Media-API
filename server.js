20d647a8afb6365ba3823489b1ff7597583d8341
OilPainter1 <hjcryns@gmail.com>
2023-03-02 12:38:36 -0600

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { User, Thought } = require('./models')
 const express = require('express')
 const router = require('./routes')
 const app = express()
-const port = 3001
+const port = 3002
 
 app.use(express.json())
 app.use(router)
